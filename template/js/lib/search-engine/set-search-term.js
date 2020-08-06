a771c3a5844a8e0e1eb3cc0ecfbee9e338020882
Matheus <matheus@e-com.club>
2020-08-06 15:09:37 -0300

    chore: search term

---
+++ template/js/lib/search-engine/set-search-term.js
@@ -0,0 +1,92 @@
+import * as merge from 'lodash.merge'
+
+export default (self, term) => {
+  const words = (term || '').split(/\s+/).map(word => {
+    switch (word) {
+      case 'iphone':
+        return 'iPhone'
+      case 'x':
+      case 'x/xs':
+        return 'X/XS'
+      case 'xs':
+        return 'XS'
+      case 'xl':
+        return 'XL'
+      case 'a20':
+      case 'a30':
+      case 'a20/a30':
+        return 'A20/A30'
+      default:
+        return word.charAt(0).toUpperCase() + word.substr(1)
+    }
+  })
+
+  // match name and/or keyword with term
+  // https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-multi-match-query.html
+  self.mergeFilter({
+    bool: {
+      should: [
+        {
+          multi_match: {
+            query: term,
+            fields: [
+              'name',
+              'keywords'
+            ]
+          }
+        },
+        {
+          bool: {
+            must: {
+              nested: {
+                path: 'specs',
+                query: {
+                  terms: {
+                    'specs.text': [
+                      words.join(' ').replace(/(samsung|apple|motorola|lg|xiaomi|huawei)\s/ig, ''),
+                      ...words
+                    ]
+                  }
+                }
+              }
+            },
+            boost: 3.5
+          }
+        }
+      ]
+    }
+  }, 'must')
+
+  merge(self.dsl, {
+    // handle terms suggestion
+    // 'did you mean?'
+    // https://www.elastic.co/guide/en/elasticsearch/reference/current/search-suggesters.html
+    suggest: {
+      text: term,
+      words: {
+        term: {
+          field: 'name'
+        }
+      }
+    }
+  })
+
+  return self
+}
+
+/**
+ * @method
+ * @name EcomSearch#setSearchTerm
+ * @description Defines term to match with product name
+ * and/or keywords on next search request.
+ *
+ * @param {string} term - Term to be searched
+ * @returns {self}
+ *
+ * @example
+// Set new search term
+search.setSearchTerm('smartphone')
+ * @example
+// Set new term and run search request
+search.setSearchTerm('notebook').fetch()
+ */
