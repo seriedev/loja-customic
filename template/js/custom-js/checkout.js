be1dec830957debe0acd4f9554925e256b307400
David Silva <david.silva@seriedesign.com.br>
2022-03-14 18:44:23 -0300

    Troca cookies por session storage

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -31,27 +31,13 @@ $(function () {
   // Funções
   // =====================
   function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-      var date = new Date();
-      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-      expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    sessionStorage.setItem(name, value);
   }
   function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(";");
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == " ") c = c.substring(1, c.length);
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
+    return sessionStorage.getItem(name);
   }
   function eraseCookie(name) {
-    document.cookie =
-      name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+    sessionStorage.removeItem(name);
   }
   function changeSedex() {
 
@@ -115,6 +101,7 @@ $(function () {
                     const elementToObserve = $(".checkout__col.col-md-6:first-child")[0];
                     const observer = new MutationObserver(function (mutations) {
                       mutations.forEach(function (mutation) {
+                        // console.log(mutation.type);
                         changeSedex();
                       });
                     });
